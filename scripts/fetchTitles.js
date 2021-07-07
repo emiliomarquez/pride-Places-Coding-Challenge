6703fae8dbd7205a3349e57b443908d4e7db000e
Emilio Marquez Fernandez <44658981+emiliomarquez@users.noreply.github.com>
2021-07-07 15:17:57 -0700

    Added script that prints out the titles of each post

--- scripts/fetchTitles.js
+++ scripts/fetchTitles.js
@@ -1,9 +1,23 @@
 
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch('https://jsonplaceholder.typicode.com/posts/')
     .then((response) => {
       return response.text();
     })
     .then((myContent) => {
-      document.querySelector('.titles').innerHTML = myContent;
+    		var obj = JSON.parse(myContent);
+    		for(let i = 0; i < obj.length; i++){
+    			document.getElementById("titles").innerHTML += JSON.stringify(obj[i].title) + "<br />";
+    		}
+    		/*
+    		var obj = JSON.parse(myContent);
+    		console.log(obj);
+    		var res = [];
+
+    		for(var i in obj)
+    			res.push(obj[i]);
+
+    		document.getElementById("titles").innerHTML = JSON.stringify(res[0].title);
+    		*/
+ 		
      
-    });
+ });
